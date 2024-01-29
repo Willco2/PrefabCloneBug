951bdc1994b40c3a3e6985d19865111f0255225e
Willco2 <willdobert@gmail.com>
2024-01-28 22:41:07 -0500

    Initial commit

---
+++ code/CubeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+
+public sealed class CubeSpawner : Component
+{
+
+	[Property] public GameObject CubePrefab { get; set; }
+
+	protected override void OnEnabled()
+	{
+		var red = CubePrefab.Clone( new Vector3( 100, 0, 25 ) ).Components.Get<ModelRenderer>();
+		var blue = CubePrefab.Clone( new Vector3( -100, 0, 25 ) ).Components.Get<ModelRenderer>();
+
+		Log.Info( $"{red.GameObject.Name} cloned with {red.GetHashCode()}" );
+		Log.Info( $"{blue.GameObject.Name} cloned with {blue.GetHashCode()}" );
+
+		red.Tint = Color.Red;
+		blue.Tint = Color.Blue;
+	}
+
+}
---
+++ code/PrefabDebug.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+public sealed class PrefabDebug : Component
+{
+
+	[Property] public ModelRenderer ModelRenderer { get; set; }
+
+	protected override void OnStart()
+	{
+		Log.Info($"{GameObject.Name} started with {ModelRenderer.GetHashCode()}");
+	}
+}
